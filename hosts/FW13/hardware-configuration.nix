556ad5d96dfbeb10d5d88d74b5d0823816c3cc4b
d3ltaaa <hil.falk@protonmail.com>
2025-09-02 11:59:26 +0200

    microcode

--- hosts/FW13/hardware-configuration.nix
+++ hosts/FW13/hardware-configuration.nix
@@ -78,5 +78,7 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.ens18.useDHCP = lib.mkDefault true;
 
+  hardware.cpu.amd.updateMicrocode = true;
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
