882a40ca8b9c677e97d6ff881a44de82e1a1321d
d3ltaaaaaa <hil.falk@protonmail.com>
2025-10-08 20:29:36 +0200

    brave fix

--- modules/applications/configurations/client/brave.nix
+++ modules/applications/configurations/client/brave.nix
@@ -1,7 +1,6 @@
 {
   lib,
   config,
-  nixpkgs-stable,
   pkgs,
   ...
 }:
@@ -22,6 +21,43 @@
       };
       programs.chromium.enable = true;
 
-      environment.systemPackages = with pkgs; [ brave ];
+      environment.systemPackages = with pkgs; [
+        (pkgs.writeShellScriptBin "brave" ''
+          exec ${pkgs.brave}/bin/brave \
+            --ozone-platform=wayland \
+            --disable-features=WaylandWpColorManagerV1 \
+            "$@"
+        '')
+        # brave crashed on hyprland when moving between monitors
+        # https://github.com/brave/brave-browser/issues/49921
+      ];
+      home-manager.users.${config.system.user.general.primary} =
+        let
+          nixos-config = config;
+        in
+        { config, ... }:
+        {
+          xdg.desktopEntries.brave = {
+            name = "Brave";
+            exec = "brave %U";
+            startupNotify = false;
+            terminal = false;
+            icon = "brave-browser";
+            genericName = "Web Browser";
+            comment = "Browse the Web securely";
+            categories = [
+              "Network"
+              "WebBrowser"
+            ];
+            mimeType = [
+              "x-scheme-handler/http"
+              "x-scheme-handler/https"
+              "x-scheme-handler/ftp"
+              "x-scheme-handler/chrome"
+              "text/html"
+              "application/x-xpinstall"
+            ];
+          };
+        };
     };
 }
