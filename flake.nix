12eff7c085815a871c01fc6f6da4805bcbe2b022
d3ltaaaaaa <hil.falk@protonmail.com>
2025-10-09 22:11:13 +0200

    fix unstable

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     pkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     # stable -> pkgs-alt
-    pkgs-alt.url = "github:nixos/nixpkgs/nixos-25.05";
+    pkgs-alt.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "pkgs";
