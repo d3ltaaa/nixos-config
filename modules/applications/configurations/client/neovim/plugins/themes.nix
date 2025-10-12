47288bfc4ef394a6d0b49a2a777a350cc5d00e3f
d3ltaaa <hil.falk@protonmail.com>
2025-10-12 12:27:31 +0200

    neovim colors

--- modules/applications/configurations/client/neovim/plugins/themes.nix
+++ modules/applications/configurations/client/neovim/plugins/themes.nix
@@ -2,28 +2,25 @@
 {
   programs.nixvim = {
     colorschemes = {
-      # base16 = {
-      #   enable = true;
-      #   colorscheme = {
-      #     base00 = "#${config.colorScheme.palette.base00}"; # #1e2030 default background
-      #     base01 = "#${config.colorScheme.palette.base01}"; # #24273a lighter background (status bars, line numbers, folding marks)
-      #     base02 = "#${config.colorScheme.palette.base02}"; # #363a4f selection background
-      #     base03 = "#${config.colorScheme.palette.base03}"; # #494d64 comments, invisibles, line highlighting
-      #     base04 = "#${config.colorScheme.palette.base04}"; # #5b6078 dark foreground
-      #     base05 = "#${config.colorScheme.palette.base05}"; # #cad3f5 default foreground, delimiters, operators
-      #     base06 = "#${config.colorScheme.palette.base06}"; # #ffffff light foreground
-      #     base07 = "#${config.colorScheme.palette.base07}"; # #b7bdf8 light background
-      #     base08 = "#${config.colorScheme.palette.base08}"; # #ed8796 variables, xml tags, markup link text
-      #     base09 = "#${config.colorScheme.palette.base09}"; # #f5a97f integers, boolean, constants
-      #     base0A = "#${config.colorScheme.palette.base0A}"; # #eed49f classes, markup bold, search text background
-      #     base0B = "#${config.colorScheme.palette.base0B}"; # #a6da95 strings, inherited class, markup code
-      #     base0C = "#${config.colorScheme.palette.base0C}"; # #8bd5ca support, regular expressions, escape characters
-      #     base0D = "#${config.colorScheme.palette.base0D}"; # #8aadf4 functions, methods, headings
-      #     base0E = "#${config.colorScheme.palette.base0E}"; # #c6a0f6 keywords, storage, selector
-      #     base0F = "#${config.colorScheme.palette.base0F}"; # #f0c6c6 deprecated
-      #   };
-      # };
       catppuccin = {
+        # palette = {
+        #   base00 = "1e2030"; # #1e2030 default background
+        #   base01 = "24273a"; # #24273a lighter background (status bars, line numbers, folding marks)
+        #   base02 = "363a4f"; # #363a4f selection background
+        #   base03 = "494d64"; # #494d64 comments, invisibles, line highlighting
+        #   base04 = "5b6078"; # #5b6078 dark foreground
+        #   base05 = "ffffff"; # #ffffff default foreground, delimiters, operators
+        #   base06 = "cad3f5"; # #cad3f5 light foreground
+        #   base07 = "b7bdf8"; # #b7bdf8 light background
+        #   base08 = "ed8796"; # #ed8796 variables, xml tags, markup link text
+        #   base09 = "f5a97f"; # #f5a97f integers, boolean, constants
+        #   base0A = "eed49f"; # #eed49f classes, markup bold, search text background
+        #   base0B = "a6da95"; # #a6da95 strings, inherited class, markup code
+        #   base0C = "8bd5ca"; # #8bd5ca support, regular expressions, escape characters
+        #   base0D = "8aadf4"; # #8aadf4 functions, methods, headings
+        #   base0E = "c6a0f6"; # #c6a0f6 keywords, storage, selector
+        #   base0F = "f0c6c6"; # #f0c6c6 deprecated
+        # };
         enable = true;
         settings = {
           # background = {
@@ -81,7 +78,8 @@
             function(colors)
               return {
                 Normal = { fg = "#${config.colorScheme.palette.base05}" },
-                Comment = { fg = "#${config.colorScheme.palette.base03}", style = { "italic" } },
+                NormalFloat = { fg = "#${config.colorScheme.palette.base05}", bg = "NONE"},
+                Comment = { fg = "#${config.colorScheme.palette.base07}", style = { "italic" } },
 
                 Constant = { fg = "#${config.colorScheme.palette.base09}" },
                 String = { fg = "#${config.colorScheme.palette.base0B}" },
@@ -89,6 +87,8 @@
                 Number = { fg = "#${config.colorScheme.palette.base09}" },
                 Boolean = { fg = "#${config.colorScheme.palette.base09}" },
                 Float = { fg = "#${config.colorScheme.palette.base09}" },
+                FloatBorder = { fg = "#${config.colorScheme.palette.base0D}", bg = "NONE" },
+                FloatTitle = { fg = "#${config.colorScheme.palette.base0D}", bg = "NONE" },
 
                 Identifier = { fg = "#${config.colorScheme.palette.base08}" },
                 Function = { fg = "#${config.colorScheme.palette.base0D}" },
@@ -116,13 +116,23 @@
                 SpecialChar = { fg = "#${config.colorScheme.palette.base0F}" },
                 Tag = { fg = "#${config.colorScheme.palette.base08}" },
                 Delimiter = { fg = "#${config.colorScheme.palette.base05}" },
-                SpecialComment = { fg = "#${config.colorScheme.palette.base03}", style = { "italic" } },
+                SpecialComment = { fg = "#${config.colorScheme.palette.base07}", style = { "italic" } },
                 Debug = { fg = "#${config.colorScheme.palette.base08}" },
 
                 Underlined = { fg = "#${config.colorScheme.palette.base0D}", style = { "underline" } },
                 Ignore = { fg = "#${config.colorScheme.palette.base00}" },
                 Error = { fg = "#${config.colorScheme.palette.base08}", bg = "#${config.colorScheme.palette.base00}", style = { "bold" } },
                 Todo = { fg = "#${config.colorScheme.palette.base0A}", bg = "#${config.colorScheme.palette.base01}", style = { "bold" } },
+
+                LineNr        = { fg = "#${config.colorScheme.palette.base06}" },
+                CursorLineNr  = { fg = "#${config.colorScheme.palette.base05}", style = { "bold" } }, 
+                VertSplit     = { fg = "#${config.colorScheme.palette.base05}" }, 
+                NvimTreeVertSplit = { fg = "#${config.colorScheme.palette.base05}" }, 
+                NvimTreeWinSeparator = { fg = "#${config.colorScheme.palette.base05}" }, 
+
+                NeoTreeVertSplit = { fg = "#${config.colorScheme.palette.base05}" }, 
+                NeoTreeStatusLineNC = { fg = "#${config.colorScheme.palette.base05}", bg = "NONE"},
+                NeoTreeWinSeparator = { fg = "#${config.colorScheme.palette.base06}"},
 
                 Added = { fg = "#${config.colorScheme.palette.base0B}" },
                 Changed = { fg = "#${config.colorScheme.palette.base0A}" },
