50cd010a5827c2da6db712d43c53a64f37494a34
d3ltaaaaaa <hil.falk@protonmail.com>
2025-10-12 21:26:06 +0200

    add ansible lsp

--- modules/applications/configurations/client/neovim/plugins/lsp.nix
+++ modules/applications/configurations/client/neovim/plugins/lsp.nix
@@ -47,9 +47,9 @@
           terraformls = {
             enable = true;
           };
-          # ansiblels = {
-          #   enable = false;
-          # };
+          ansiblels = {
+            enable = false;
+          };
           jsonls = {
             enable = true;
           };
@@ -74,9 +74,9 @@
                     kubernetes = "'*.yaml";
                     "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
                     "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
-                    # "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
+                    "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
                     "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
-                    # "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
+                    "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
                     "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
                     "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
                     "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" =
@@ -144,7 +144,7 @@
       };
     };
     extraPlugins = with pkgs.vimPlugins; [
-      # ansible-vim
+      ansible-vim
     ];
 
     extraConfigLua = ''
