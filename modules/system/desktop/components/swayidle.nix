3b7a744252ddd31a4bfe3234ae53e3f7d78ce1f5
d3ltaaa <hil.falk@protonmail.com>
2025-10-19 00:06:09 +0200

    swayidle fix

--- modules/system/desktop/components/swayidle.nix
+++ modules/system/desktop/components/swayidle.nix
@@ -28,30 +28,34 @@
               package = pkgs.swayidle;
               timeouts = [
                 {
-                  timeout = 180;
-                  command = "'notify-send 'Locking in 5 seconds' -t 5000'";
+                  timeout = 30;
+                  command = "${pkgs.dunst}/bin/dunstify 'Locking in 5 seconds'";
                 }
 
                 {
-                  timeout = 185;
-                  command = "swaylock";
+                  timeout = 35;
+                  command = "${pkgs.hyprlock}/bin/hyprlock &";
                 }
 
                 {
-                  timeout = 190;
-                  command = "swaymsg 'output * dpms off'";
-                  resumeCommand = "swaymsg 'output * dpms on'";
+                  timeout = 50;
+                  command = "${pkgs.niri}/bin/niri msg action power-off-monitors";
+                  resumeCommand = "${pkgs.niri}/bin/niri msg action power-on-monitors";
                 }
 
                 {
-                  timeout = 195;
-                  command = "'systemctl suspend'";
+                  timeout = 60;
+                  command = "${pkgs.systemd}/bin/systemctl suspend";
                 }
               ];
               events = [
                 {
                   event = "before-sleep";
-                  command = "swaylock";
+                  command = "${pkgs.hyprlock}/bin/hyprlock &";
+                }
+                {
+                  event = "after-resume";
+                  command = "${pkgs.niri}/bin/niri msg action power-on-monitors";
                 }
               ];
             };
