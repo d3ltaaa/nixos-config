ab6ce0a91aefe13d3c7f16679ee859b1a245bb29
d3ltaaa <hil.falk@protonmail.com>
2025-10-19 00:12:44 +0200

    times

--- modules/system/desktop/components/swayidle.nix
+++ modules/system/desktop/components/swayidle.nix
@@ -19,6 +19,7 @@
       home-manager.users.${config.system.user.general.primary} =
         let
           nixos-cfg = cfg;
+          nixos-components = config.system.desktop.components;
         in
         { ... }:
         {
@@ -28,34 +29,53 @@
               package = pkgs.swayidle;
               timeouts = [
                 {
-                  timeout = 30;
-                  command = "${pkgs.dunst}/bin/dunstify 'Locking in 5 seconds'";
+                  timeout = 120;
+                  command =
+                    if nixos-components.dunst.enable then "${pkgs.dunst}/bin/dunstify 'Locking in 20 seconds'" else "";
                 }
 
                 {
-                  timeout = 35;
-                  command = "${pkgs.hyprlock}/bin/hyprlock &";
+                  timeout = 140;
+                  command = if nixos-components.hyprlock.enable then "${pkgs.hyprlock}/bin/hyprlock &" else "";
                 }
 
                 {
-                  timeout = 50;
-                  command = "${pkgs.niri}/bin/niri msg action power-off-monitors";
-                  resumeCommand = "${pkgs.niri}/bin/niri msg action power-on-monitors";
+                  timeout = 200;
+                  command =
+                    if nixos-components.niri.enable then
+                      "${pkgs.niri}/bin/niri msg action power-off-monitors"
+                    else if nixos-components.hyprland.enable then
+                      "hyprctl dispatch dpms off"
+                    else
+                      "";
+                  resumeCommand =
+                    if nixos-components.niri.enable then
+                      "${pkgs.niri}/bin/niri msg action power-on-monitors"
+                    else if nixos-components.hyprland.enable then
+                      "${pkgs.hyprland}/bin/hyprctl dispatch dpms on && ${pkgs.brightnessctl}/bin/brightnessctl -r"
+                    else
+                      "";
                 }
 
                 {
-                  timeout = 60;
+                  timeout = 260;
                   command = "${pkgs.systemd}/bin/systemctl suspend";
                 }
               ];
               events = [
                 {
                   event = "before-sleep";
-                  command = "${pkgs.hyprlock}/bin/hyprlock &";
+                  command = if nixos-components.hyprlock.enable then "${pkgs.hyprlock}/bin/hyprlock &" else "";
                 }
                 {
                   event = "after-resume";
-                  command = "${pkgs.niri}/bin/niri msg action power-on-monitors";
+                  command =
+                    if nixos-components.niri.enable then
+                      "${pkgs.niri}/bin/niri msg action power-on-monitors"
+                    else if nixos-components.hyprland.enable then
+                      "${pkgs.hyprland}/bin/hyprctl dispatch dpms on && ${pkgs.brightnessctl}/bin/brightnessctl -r"
+                    else
+                      "";
                 }
               ];
             };
