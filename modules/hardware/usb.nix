f818b1f4bcbb6c16f03743dc8b129d1d0c516445
d3ltaaa <hil.falk@protonmail.com>
2025-10-18 16:21:39 +0200

    udiskie

--- modules/hardware/usb.nix
+++ modules/hardware/usb.nix
@@ -18,5 +18,18 @@
     lib.mkIf cfg.enable {
       services.udisks2.enable = true;
       services.udisks2.package = pkgs.udisks2;
+      home-manager.users.${config.system.user.general.primary} =
+        { ... }:
+        {
+          services.udiskie = {
+            enable = true;
+            settings = {
+              program_options = {
+                # replace with your favorite file manager
+                file_manager = "${pkgs.nautilus}/bin/nautilus";
+              };
+            };
+          };
+        };
     };
 }
